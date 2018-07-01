2a62b2abe834e35e35910d16dfadb09fd79e81a4
swissjov <40548911+swissjov@users.noreply.github.com>
2018-07-01 20:14:14 +0300

    My app layout
    Due to the shortage of time and other things I encountered, I couldn't learn enough to fully make this app functional.

---
+++ MainActivity.java
@@ -0,0 +1,17 @@
+package com.mycompany.myapp;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+		
+		
+		
+    }
+}
